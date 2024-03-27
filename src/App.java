681046a9de768f713edcdb0848ca044c4a5329f4
sadhubby <iamthegreatgamerx.silent@gmail.com>
2024-03-27 13:55:52 +0800

    Fully updated Main Menu All possible features in main menu has been put in. Methods for each case are left to do.

--- src/App.java
+++ src/App.java
@@ -26,18 +26,25 @@ public class App {
    
     int stopper = 1;
     while(stopper == 1){
-     
 
+        System.out.println("===========================");
+        System.out.println("MAIN MENU");
         System.out.println("===========================\n");
         System.out.println("1. Records Management");
         System.out.println("2. Transaction Management");
-        System.out.println("3. Exit Program\n");
+        System.out.println("3. Record Generation");
+        System.out.println("4. Exit\n");
         System.out.println("===========================");
         
+
         System.out.print("Enter choice:");
         int inputForMainMenu = sc.nextInt();
         System.out.println("===========================");
-        if (inputForMainMenu == 1){
+
+
+        if (inputForMainMenu == 1){// record management
+                System.out.println("RECORD MANAGEMENT MENU");
+                System.out.println("===========================");
                 System.out.println("\n1. Products");
                 System.out.println("2. Customers");
                 System.out.println("3. Employee");
@@ -48,6 +55,8 @@ public class App {
                 int recordsInput = sc.nextInt();
                 switch (recordsInput) {
                     case 1: //products
+                        System.out.println("===========================");
+                        System.out.println("PRODUCT MANAGEMENT MENU");
                         System.out.println("===========================\n");
                         System.out.println("1. Create a new Product");
                         System.out.println("2. Update a record of a Product");
@@ -58,22 +67,41 @@ public class App {
                         int productsInput = sc.nextInt();
                         switch (productsInput) {
                             case 1: // Create new product
+
+
+                            
                             
                                 break;
                         
                             case 2: // Update record of Product
+
+
+
+
                                 break;
                             
                             case 3: //delete a record of product
+
+
+
+
                                 break;
                             case 4: // View product and order list given a  specific year
+
+
+
+
                                 break;
                             default:
-                               break; 
+                                System.out.println("===========================\n");
+                                System.out.println("Invalid choice. Going back to main menu\n");
+                                break; 
                             }
                         break;
                     case 2: // customers 
                     // since this is not mine anymore, edit the menu as you please
+                        System.out.println("===========================");
+                        System.out.println("CUSTOMERS MANAGEMENT MENU");
                         System.out.println("===========================\n");
                         System.out.println("1. Create a new Customer");
                         System.out.println("2. Update a record of Customer");
@@ -85,20 +113,38 @@ public class App {
                         switch (customersInput) {
                             case 1:
                                 
+
+
                                 break;
+
                             case 2:
+
+
+
+
                                 break;
                             case 3:
+
+
+
+
                                 break;
                             case 4:
+
+
+
+
                                 break;
                             default:
+                                System.out.println("===========================\n");
+                                System.out.println("Invalid choice. Going back to main menu\n");
                                 break;
-                          
                         }
                         break;
                     case 3: // employee 
                     // since this is not mine anymore, edit the menu as you please
+                        System.out.println("===========================");
+                        System.out.println("EMPLOYEES MANAGEMENT MENU");
                         System.out.println("===========================\n");
                         System.out.println("1. Create a new Employee");
                         System.out.println("2. Update a record of Employee");
@@ -118,11 +164,15 @@ public class App {
                             case 4:
                                 break;
                             default:
+                                System.out.println("===========================\n");
+                                System.out.println("Invalid choice. Going back to main menu\n");
                                 break;
                             }
                         break;
                     case 4: //office
                     // since this is not mine anymore, edit the menu as you please
+                        System.out.println("===========================");
+                        System.out.println("OFFICE MANAGEMENT MENU");
                         System.out.println("===========================\n");
                         System.out.println("1. Create a new Office");
                         System.out.println("2. Update a record of an Office");
@@ -134,32 +184,101 @@ public class App {
                         switch (officesInput) {
                             case 1:
                                 
+
                                 break;
                         
                             case 2:
+
+
                                 break;
                             case 3:
+
+
                                 break;
                             case 4:
+
+
                                 break;
                             default:
+                                System.out.println("===========================\n");
+                                System.out.println("Invalid choice. Going back to main menu\n");
                                 break;
                             }
                         break;
+
+
+
+
+
                     default:
+                        System.out.println("===========================\n");
+                        System.out.println("Invalid choice. Going back to main menu\n");
+                }
+
+        }
+        else if(inputForMainMenu == 2){//transaction management
+                System.out.println("TRANSACTION MANAGEMENT MENU");
+                System.out.println("===========================");
+                System.out.println("\n1. Orders management");
+                System.out.println("2. Payment management\n");
+                System.out.println("===========================");
+                System.out.print("Enter choice:");
+                int inputForTransanction = sc.nextInt();
+                switch (inputForTransanction) {
+                    case 1: //orders
+                        
+                        break;
+                
+                    case 2: //payment
+
+
+                        break;
+
+
+
+
+
+                    default:
+                        System.out.println("===========================\n");
+                        System.out.println("Invalid choice. Going back to main menu\n");
                         break;
                 }
 
         }
-        else if(inputForMainMenu == 2){
-                System.out.println("2");
+        else if(inputForMainMenu == 3){ //report generation
+                System.out.println("REPORT GENERATION MENU");
+                System.out.println("===========================");
+                System.out.println("\n1. Sales Report for different sales per status");
+                System.out.println("2. Sales Report for different sales per product\n");
+                System.out.println("===========================");
+                System.out.print("Enter choice:");
+                int inputForReports = sc.nextInt();
+                switch (inputForReports) {
+                    case 1:
+                        
+                        break;
+                
+                    case 2:
+
+
+                        break;
+                    
+                        
+
+
+                    default:
+                        System.out.println("===========================\n");
+                        System.out.println("Invalid choice. Going back to main menu\n");
+                        break;
+                }
         }
-        else if(inputForMainMenu == 3){
-                System.out.println("Ending program...");
+        else if(inputForMainMenu == 4){
+                System.out.println("\nEnding program...\n");
+                System.out.println("===========================");
                 stopper = 0;
         }
         else{
-                System.out.println("Invalid choice");
+                System.out.println("\nInvalid choice\n");
         }
         }// end of while loop
     sc.close();
